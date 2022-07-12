a3eb93dea30fedf6e4d53bd6baa5964946e924d8
Loyed Eldon Crasta <loyedcrasta17@gmail.com>
2022-07-12 15:58:13 +0530

    Code to Trime Array to Size

---
+++ src/TrimToArrayListSize.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrimToArrayListSize {
+    public static void main(String[] args) {
+        List<String> lis1 = new ArrayList<>();
+        lis1.add("Red");
+        lis1.add("Green");
+        lis1.add("Blue");
+        lis1.add("Orange");
+        lis1.add("Black");
+
+        System.out.println(lis1);
+
+        ((ArrayList<String>) lis1).trimToSize();
+        System.out.println(lis1);
+
+    }
+}
+
